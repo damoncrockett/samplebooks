13724f5090c664563af63a2c959f4f7680997c9a
damoncrockett <damoncrockett@hotmail.com>
2024-10-18 23:07:43 -0400

    basic

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import imgpaths from '../assets/json/imgpaths.json';
+
+export function returnDomain() {
+    const production = process.env.NODE_ENV === 'production';
+    return production ? '' : 'http://localhost:8888/'
+}
+
+export default function App() {
+    const [imgPath, setImgPath] = useState(imgpaths[0]);
+
+    return (
+        <div>
+            <div id='controls'>
+                <button
+                    id='rand'
+                    onClick={() => {
+                        const randomIndex = Math.floor(Math.random() * imgpaths.length);
+                        setImgPath(imgpaths[randomIndex]);
+                    }}
+                >
+                    RAND
+                </button>
+                <div id='imgpath'>{imgPath.slice(0,-4)}</div>
+            </div>
+            <img
+                src={returnDomain() + imgPath}
+            />
+        </div>
+    );
+}
