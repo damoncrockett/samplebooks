8759d3f932817356bb5dcb5b8a13c3cafc9f756b
damoncrockett <damoncrockett@hotmail.com>
2024-10-18 23:11:37 -0400

    img/

--- src/components/App.js
+++ src/components/App.js
@@ -24,7 +24,7 @@ export default function App() {
                 <div id='imgpath'>{imgPath.slice(0,-4)}</div>
             </div>
             <img
-                src={returnDomain() + imgPath}
+                src={returnDomain() + 'img/' + imgPath}
             />
         </div>
     );
